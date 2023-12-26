385ea4acbdb971bd76bda1aa2a289f7c7227e13f
HodayaGitHub <hodaya1812@gmail.com>
2023-12-26 10:01:45 +0200

    Add more data to the toy JSON & add update and delete to the server

--- server.js
+++ server.js
@@ -76,6 +76,51 @@ app.post('/api/toy', (req, res) => {
 })
 
 
+// toy UPDATE
+app.put('/api/toy', (req, res) => {
+    const loggedinUser = userService.validateToken(req.cookies.loginToken)
+    if (!loggedinUser) return res.status(401).send('Cannot update item')
+    const item = {
+        _id: req.body._id,
+        vendor: req.body.vendor,
+        speed: +req.body.speed,
+        price: +req.body.price,
+    }
+    toyService.save(item, loggedinUser)
+        .then((savedItem) => {
+            res.send(savedItem)
+        })
+        .catch((err) => {
+            loggerService.error('Cannot save item', err)
+            res.status(400).send('Cannot save item')
+        })
+
+})
+
+
+// toy DELETE
+app.delete('/api/toy/:toyId', (req, res) => {
+    const loggedinUser = userService.validateToken(req.cookies.loginToken)
+    loggerService.info('loggedinUser item delete:', loggedinUser)
+    if (!loggedinUser) {
+        loggerService.info('Cannot remove item, No user')
+        return res.status(401).send('Cannot remove item')
+    }
+
+    const { itemId } = req.params
+    itemService.remove(itemId, loggedinUser)
+        .then(() => {
+            loggerService.info(`item ${itemId} removed`)
+            res.send('Removed!')
+        })
+        .catch((err) => {
+            loggerService.error('Cannot remove item', err)
+            res.status(400).send('Cannot remove item')
+        })
+
+})
+
+
 app.get('/**', (req, res) => {
     res.sendFile(path.resolve('public/index.html'))
 })
