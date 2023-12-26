0bc156e832b0aef9f0aa58967dbb88c2fac2505c
HodayaGitHub <hodaya1812@gmail.com>
2023-12-26 15:14:59 +0200

    Adjustment for the port before uploading the project to render

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/**', (req, res) => {
 })
 
 
-const PORT = 3030
-app.listen(PORT, () =>
+const port = process.env.PORT || 3030
+app.listen(port, () =>
     loggerService.info(`Server listening on port http://127.0.0.1:${PORT}/`)
 )
